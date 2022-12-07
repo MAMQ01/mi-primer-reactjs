a069d61eeb2fda6669ec5c612b9a1160a4170cae
Manuel Andres Muñoz <andresmq2018@gmail.com>
2022-12-07 15:51:49 +0100

    actualizaciones

---
+++ src/components/navBar/NavBar.jsx
@@ -0,0 +1,22 @@
+import "./NavBar.css"
+
+const NavBar = ()=> {
+
+    return(
+        <div>
+            <div>
+                <h1>Restaurante</h1>
+                <ul >
+                    <li>Sushi</li>
+                    <li>Comida italina</li>
+                    <li>Comida china</li>
+                    <li>Comida intanfil</li>
+                </ul>
+                
+            </div>
+        </div>
+        
+    )
+}
+
+export default NavBar;

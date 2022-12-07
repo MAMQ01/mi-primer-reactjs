9eaf5c7ccda30436d966480e9f22371dd9bf8a37
Manuel Andres Muñoz <andresmq2018@gmail.com>
2022-12-07 20:48:01 +0100

    agregando un mensaje de bienvenida

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -45,9 +45,12 @@ const NavBar = () => {
                         </div>
                     </div>
                 </nav>
-
-
-
+            </div>
+            <div className="containerBienvenida">
+                <button data-text="Awesome" class="button">
+                    <span class="actual-text">&nbsp;Bienvenido&nbsp;</span>
+                    <span class="hover-text" aria-hidden="true">&nbsp;Bienvenido&nbsp;</span>
+                </button>
             </div>
         </div>
     );

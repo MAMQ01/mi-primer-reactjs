bb1a59a0039234bf1bf44c9bed72a0e460c406b9
Manuel Andres Muñoz <andresmq2018@gmail.com>
2022-12-08 13:58:49 +0100

    sin comentarios

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import NavBar from "./components/navBar/NavBar"
 import ItemListContainer from "./components/itemListContainer/ItemListContainer";
 import CardWidget from "./components/cardWidget/CardWidget";
 
-function App () {
+function App() {
   return (
     <div>
       <NavBar />
-      <ItemListContainer greeting={"Revisa los platos recomendados"}/>
+      <ItemListContainer greeting={"Revisa los platos recomendados"} />
     </div>
   );
 }
